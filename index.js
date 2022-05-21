b067f904078ea0e52e2c3080276b745decf72b44
Ske1l <hayley6236@gmail.com>
2022-05-21 10:37:29 -0400

    JS is hard

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(url, left, bottom) {
+    let item = document.createElement('img')
+    item.src = url
+    item.style.position = 'fixed'
+    item.style.left = left + 'px'
+    item.style.bottom = bottom + 'px'
+    document.body.append(item)
+
+    return item
+}
+
+
+function newItem(url, left, bottom) {
+    let item = newImage(url, left, bottom)
+    item.addEventListener('dblclick', function () {
+        item.remove()
+    })
+}
+
+
+
+
+newImage('assets/green-character.gif', 100, 250)
+newImage('assets/pine-tree.png', 450, 350)
+newImage('assets/tree.png', 200, 450)
+newImage('assets/pillar.png', 350, 250)
+newImage('assets/crate.png', 150, 350)
+newImage('assets/well.png', 500, 575)
+
+
+
+newItem('assets/sword.png', 500, 555)
+newItem('assets/sheild.png', 165, 335)
+newItem('assets/staff.png', 600, 250)
